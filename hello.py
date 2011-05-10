05f2a9f5be624be16f8ba82272032a9c48e191a2
jrob <jrob@hitpost.com>
2011-05-10 11:52:43 -0700

    adding hello

---
+++ hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# just a placeholder python file!
+
+from optparse import OptionParser
+import os
+import re
+import sys, urllib2, urllib, socket, time, datetime
+from random import randint
+
+def main():
+    print 'hello, world'
+
+if __name__ == '__main__':
+    main()
+
+
+
